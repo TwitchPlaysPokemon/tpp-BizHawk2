14122e700d93dcc8447ca44a0e4c3578df6983b3
m4-used-rollout <dave.was.r@gmail.com>
2020-02-14 03:35:36 +0000

    Added simple FS Lua library for directory listings so you don't need popen

---
+++ BizHawk.Client.Common/lua/EmuLuaLibrary.FS.cs
@@ -0,0 +1,27 @@
+﻿using NLua;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizHawk.Client.Common.lua
+{
+	public sealed class FSEmuLuaLibrary : LuaLibraryBase
+	{
+		public FSEmuLuaLibrary(Lua lua)
+	: base(lua) { }
+
+		public FSEmuLuaLibrary(Lua lua, Action<string> logOutputCallback)
+			: base(lua, logOutputCallback) { }
+
+		public override string Name { get { return "fs"; } }
+
+		[LuaMethod(
+			"getfiles",
+			"Loads a lua table of files in a given directory. Can be given a search string as well."
+		)]
+		public LuaTable GetFiles(string path = ".", string search = null) => (string.IsNullOrWhiteSpace(search) ? Directory.GetFiles(path) : Directory.GetFiles(path, search)).ToLuaTable(Lua);
+	}
+}
