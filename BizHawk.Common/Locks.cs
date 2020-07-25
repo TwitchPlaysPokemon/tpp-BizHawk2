6d25b079e2fc3de63b703543fcfc5b6afe1b22e1
Chaos-lord <dd4791.mariokarter@hotmail.co.uk>
2020-07-25 21:45:54 +0000

    apparently git commit -a does not add files

---
+++ BizHawk.Common/Locks.cs
@@ -0,0 +1,7 @@
+﻿namespace BizHawk.Common
+{
+	public static class Locks //added as psuedo-global as there was no other way to get the scope to the http event where it is needed
+	{
+		public static readonly object LuaLock = new object();
+	}
+}
